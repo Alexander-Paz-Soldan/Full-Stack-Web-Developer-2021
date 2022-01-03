a20f7b6a36666c5479374f69e61a8f3ba53d0cbd
Alexander Paz Soldan <sunztualex@hotmail.com>
2022-01-03 20:12:48 +0100

    workaround dateutil parsing error

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -62,7 +62,8 @@ class Artist(db.Model):
 #----------------------------------------------------------------------------#
 
 def format_datetime(value, format='medium'):
-  date = dateutil.parser.parse(value)
+    # TODO: this causes an error
+  date = "today" # dateutil.parser.parse(value)
   if format == 'full':
       format="EEEE MMMM, d, y 'at' h:mma"
   elif format == 'medium':
